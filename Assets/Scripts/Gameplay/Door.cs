517dae6ee9ec0ccee14ffac34a3efd08e766e693
Weston Mcnamara <westonfmcnamara@gmail.com>
2024-10-20 15:07:32 -0400

    add doors

---
+++ Assets/Scripts/Gameplay/Door.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[DisallowMultipleComponent]
+public class Door : MonoBehaviour, IInteractable
+{
+    public bool IsOpen { get; private set; } = false;
+
+    public float OpenSpeed = 0.5f;
+
+    [SerializeField] private AudioClip doorOpenSfx;
+    [SerializeField] private bool switchOpenDirection;
+
+    private Vector3 doorOpenRot;
+    private Vector3 doorClosedRot;
+    private AudioSource doorSource;
+    private float progress;
+
+    public void Start()
+    {
+        doorSource = GetComponent<AudioSource>();
+        doorSource.spatialBlend = 1.0f;
+
+        doorClosedRot = transform.localRotation.eulerAngles;
+        doorOpenRot = doorClosedRot;
+        doorOpenRot.y += switchOpenDirection ? -90f : 90.0f;
+
+        progress = 0.0f;
+    }
+
+    private void Update()
+    {
+        Vector3 lerpTarget = IsOpen ? doorOpenRot : doorClosedRot;
+
+        progress = Mathf.Clamp01(progress + Time.deltaTime * OpenSpeed);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(lerpTarget), progress);
+    }
+
+    public void Interact(InteractData interactData)
+    {
+        IsOpen = !IsOpen;
+        progress = 0.0f;
+        doorSource.PlayOneShot(doorOpenSfx);
+    }
+
+    public bool CanBeInteractedWith()
+    {
+        return true;
+    }
+}
