a591bcf19d47d794e68e16911e317a70f167b1c2
Weston Mcnamara <westonfmcnamara@gmail.com>
2024-10-21 15:01:36 -0400

    Add main menu

---
+++ Assets/Scripts/MenuGUIFunctionHelper.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGUIFunctionHelper : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
